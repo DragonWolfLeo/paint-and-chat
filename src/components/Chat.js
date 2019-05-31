adee6a919f8c05c3acb6a2c9306cc3cc1dbe7911
Dragon Wolf Leo <dleowolf@gmail.com>
2019-05-31 02:16:45 -0500

    active_users is stored as a linked list

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,7 @@
 import React from "react";
 import * as api from '../api/api';
 import '../css/Chat.css';
+import LinkedList from '../util/LinkedList';
 
 // Constants
 const MESSAGE_TYPES = Object.freeze({
@@ -11,60 +12,6 @@ const MESSAGE_TYPES = Object.freeze({
 	NEW_MESSAGE: "new_message",
 });
 
-class Node {
-	constructor(value, next = null, previous = null){
-		this.value = value;
-		this.next = next;
-		this.previous = previous;
-	}
-}
-class LinkedList {
-	constructor(){
-		this.head = null;
-		this.tail = null;
-		this.length = 0;
-	}
-	append(value){
-		const node = new Node(value, null, this.tail);
-		if(!this.length){
-			this.head = node;
-		} else {
-			this.tail.next = node;
-		}
-		this.tail = node;
-		this.length++;
-		return node;
-	}
-	prepend(value){
-		const node = new Node(value, this.head);
-		if(!this.length){
-			this.tail = node;
-		} else {
-			this.head.previous = node;
-		}
-		this.head = node;
-		this.length++;
-		return node;
-	}
-	detach(node){
-		if(!node){ return }
-		const {next, previous} = node;
-		node.next = null;
-		node.previous = null;
-		previous.next = next;
-		next.previous = previous;
-		return node;
-	}
-	toArray(){
-		const arr = [];
-		let next = this.head;
-		while(next){
-			arr.push(next.value);
-			next = next.next;
-		}
-		return arr;
-	}
-}
 
 class Chat extends React.Component {
 	constructor(){
