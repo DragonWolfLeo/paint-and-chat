471e87db830f7bac28c5a1a5d86ea3d7acc18c94
Dragon Wolf Leo <dleowolf@gmail.com>
2019-06-20 04:51:07 -0500

    Add github link footer

--- src/components/WelcomeScreen.js
+++ src/components/WelcomeScreen.js
@@ -5,12 +5,20 @@ import Signin from './Signin';
 const WelcomeScreen = props => {
 	const {awaiting, room, joinRoom, declineRoom} = props;
 	return (
-		<header className="welcome flex flex-wrap justify-center items-center">
-			<div className="logoContainer mh3 mb3">
-				<img className="logo" alt="logo" src="/images/logo.png" />
+			<div className="welcomeContainer flex flex-column">
+				<header className="welcome flex flex-wrap items-center justify-center mb5">
+					<div className="logoContainer mh3 mb3">
+						<img className="logo" alt="logo" src="/images/logo.png" />
+					</div>
+					{!awaiting && <Signin room={room} joinRoom={joinRoom} declineRoom={declineRoom}/>}
+				</header>
+				<footer>
+					<a className="white link" href="https://github.com/DragonWolfLeo/paint-and-chat">
+						<ion-icon name="logo-github" />
+						<span className="pl2">View on GitHub</span>
+					</a>
+				</footer>
 			</div>
-			{!awaiting && <Signin room={room} joinRoom={joinRoom} declineRoom={declineRoom}/>}
-		</header>
 	);
 	
 }
